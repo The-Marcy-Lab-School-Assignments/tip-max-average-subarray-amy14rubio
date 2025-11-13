786646c028cb69b05055eb541fa577beb8c2528d
amy14rubio <amy14rubio@gmail.com>
2025-11-13 11:08:55 -0500

    solved

--- example.js
+++ example.js
@@ -1,30 +1,33 @@
 /**
  * Brute Force Solution
- * 
- * Time Complexity: 
+ *
+ * Time Complexity:
  * Space Complexity:
- * 
+ *
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 function findMaxAverage(nums, k) {
-    let maxAverage = -Infinity;
-    
-    for (let i = 0; i <= nums.length - k; i++) {
-        let sum = 0;
-        for (let j = i; j < i + k; j++) {
-            sum += nums[j];
-        }
-        
-        const average = sum / k;
-        if (average > maxAverage) {
-            maxAverage = average;
-        }
+  // variable to store max
+  let maxAverage = -Infinity;
+
+  // sets the start of the subarray according to the amount that it permits
+  for (let i = 0; i <= nums.length - k; i++) {
+    let sum = 0; // variable to store the sum of the nums
+    // iterates through each subarray only up to the end of the subarray
+    for (let j = i; j < i + k; j++) {
+      sum += nums[j]; // adds the nums to current subarray sum
     }
-    
-    return maxAverage;
+
+    const average = sum / k; // stores what the average is of the nums
+    if (average > maxAverage) {
+      // only if the average is greater than maxAverage
+      maxAverage = average; // current subarray average is stored to the maxAverage
+    }
+  }
+
+  return maxAverage;
 }
 
 module.exports = findMaxAverage;
-
--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 /**
  * Find the maximum average value of a contiguous subarray of length k
- * 
- * Time Complexity: 
+ *
+ * Time Complexity:
  * Space Complexity:
- * 
+ *
  * @param {number[]} nums - An integer array consisting of n elements
  * @param {number} k - The length of the contiguous subarray
  * @return {number} - The maximum average value
  */
-function findMaxAverage(nums, k) {
-    // TODO: Implement your solution here
-    
 
+function findMaxAverage(nums, k) {
+  let sum = nums.slice(0, k).reduce((acc, curr) => acc + curr); // O (k)
+  let maxAverage = sum / k;
+
+  // O (n) <-- this is the longer array so this is O(n)
+  for (let i = 0; i < nums.length - k; i++) {
+    sum = sum - nums[i] + nums[i + k];
+    maxAverage = Math.max(maxAverage, sum / k);
+  }
+
+  return maxAverage;
 }
 
-module.exports = findMaxAverage;
+let nums = [1, 2, 3, 4, 1];
 
+console.log(findMaxAverage(nums, 3));
+
+module.exports = findMaxAverage;
